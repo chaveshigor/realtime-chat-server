23a06e243b5e1b87c839d7c75c441dcf52525bba
Higor Chaves <hhigorsilvachaves@gmail.com>
2023-02-25 16:58:35 -0300

    chore: add docker containers

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import express from 'express';
-import router from './routes';
-import * as dotenv from 'dotenv';
+import app from './app';
 
-dotenv.config();
-
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(router);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
