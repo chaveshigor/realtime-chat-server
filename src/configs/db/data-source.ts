25e2d2fcb09a590f14d1c44f7be5262781975938
Higor Chaves <hhigorsilvachaves@gmail.com>
2023-02-25 19:12:28 -0300

    fix: add db host to env

--- src/configs/db/data-source.ts
+++ src/configs/db/data-source.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "db",
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
