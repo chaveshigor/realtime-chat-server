082e0e9427f43b7a5c21efb069467e4043162444
Higor Chaves <hhigorsilvachaves@gmail.com>
2023-02-25 16:08:19 -0300

    chore: add router file and dotenv lib

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello world');
+import express from 'express';
+import router from './routes';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
