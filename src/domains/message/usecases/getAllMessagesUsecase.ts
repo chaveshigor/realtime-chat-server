e9ffe709d4528697cd98e3ab512a11dcc26af153
Higor Chaves <hhigorsilvachaves@gmail.com>
2023-02-26 14:25:20 -0300

    fix: get user with messages

--- src/domains/message/usecases/getAllMessagesUsecase.ts
+++ src/domains/message/usecases/getAllMessagesUsecase.ts
@@ -1,14 +1,11 @@
 import { ApplicationIUsecase } from "../../../shared/interfaces/usecase";
 import ApplicationUseCase from "../../../shared/classes/applicationUsecase";
 import { Message } from "../../../entities/message";
-import { Chat } from "../../../entities/chat";
 
 class GetAllMessagesUsecase extends ApplicationUseCase implements ApplicationIUsecase {
   async run(chatId: number): Promise<Message[]> {
-    const chatRepository = this.getRepository(Chat);
-
-    const chat = await chatRepository.findOne({ relations: { messages: true }, where: { id: chatId } }) as Chat;
-    const messages = chat.messages
+    const messageRepository = this.getRepository(Message);
+    const messages = await messageRepository.find({where: { chat: {id: chatId} }, relations: {user: true}});
 
     return messages;
   };
