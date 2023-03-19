97357f508c748af23463ba2e3625ff95580310fb
Higor Chaves <hhigorsilvachaves@gmail.com>
2023-03-18 21:46:02 -0300

    fix: fix variable to camelcase

--- src/domains/chat/usecases/findOrCreateUsecase.ts
+++ src/domains/chat/usecases/findOrCreateUsecase.ts
@@ -22,16 +22,16 @@ class CreateOrCreateUsecase extends ApplicationUseCase implements ApplicationIUs
       );
       return chat as Chat;
     }else {
-      const new_chat = chatRepository.create();
-      await chatRepository.save(new_chat);
+      const newChat = chatRepository.create();
+      await chatRepository.save(newChat);
 
       (params.member_ids as Array<number>).forEach(async userId => {
         const user = await userRepository.findOne({where: {id: userId}, relations: {chats: true} }) as User;
-        user.chats = [...user.chats, new_chat];
+        user.chats = [...user.chats, newChat];
         await userRepository.save(user);
       });
 
-      return new_chat;
+      return newChat;
     }
   }
 }
